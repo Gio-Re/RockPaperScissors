737b13ded93714385c07883899b3aa19ca271b69
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-03-18 20:36:02 +0100

    Create a new function to manage the play round
    nested swich manage the result of the round, keep tracx of score and consol log the result of the round

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* create a  function that for the value of math.random return the computer choiche contained in an array*/ 
+
 function getComputerChoice() {
     let randomChoice = Math.random();
     let gameChoice = ["rock", "paper", "scissors"];
@@ -11,9 +12,69 @@ function getComputerChoice() {
     } 
 };
 console.log(getComputerChoice());
+
 /*create a function to take the human choice*/
 
 function getHumanChoice() {
     return prompt("Rock, Paper or Scissor: ").toLowerCase();
 }
 console.log(getHumanChoice());
+
+// create 2 variable to keep track of human and computer score
+
+let humanScore = 0;
+let computerScore = 0;
+
+// create a new function to play the round
+
+function playRound(computerChoice, humanChoice) {
+    switch (computerChoice) {
+        case "rock":
+            switch (humanChoice) {
+                case "rock":
+                    console.log("It's a Draw! Rock vs Rock.");
+                    break;
+                case "paper":
+                    console.log("You Win! Paper beats Rock.");
+                    humanScore += 1;
+                    break;
+                case "scissors":
+                    console.log("You Lose! Rock beats Scissors!");
+                    computerScore += 1;
+                    break; 
+            }
+        case "paper":
+            switch (humanChoice) {
+                case "rock":
+                    console.log("You Lose! Paper beats Rock.");
+                    computerScore += 1;
+                    break;
+                case "paper":
+                    console.log("It's a Draw. Paper vs Paper.");
+                    break;
+                case "scissors":
+                    console.log("You Win! Scissors beats Paper!");
+                    humanScore += 1;
+                    break; 
+            }
+        case "scissors":
+            switch (humanChoice) {
+                case "rock":
+                    console.log("You Win! Rock beats Scissors.");
+                    humanScore += 1;
+                    break;
+                case "paper":
+                    console.log("You Lose! Scissors beats Paper.");
+                    computerScore += 1;
+                    break;
+                case "scissors":
+                    console.log("It's a Draw! Scissors vs Scissors!");
+                    break; 
+            }
+            
+    }
+}
+
+playRound(getComputerChoice(), getHumanChoice());
+console.log(computerScore);
+console.log(humanScore);
