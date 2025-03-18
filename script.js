b689ac26887123e2674589fff7946c5aeadce958
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-03-18 19:39:41 +0100

    Create a funciotn to take the human choice
    the function return the choice lower case to make the game case insensitive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* create a  function that for the value of math.random return the computer choiche contained in an array*/ 
 function getComputerChoice() {
     let randomChoice = Math.random();
     let gameChoice = ["rock", "paper", "scissors"];
@@ -10,3 +11,9 @@ function getComputerChoice() {
     } 
 };
 console.log(getComputerChoice());
+/*create a function to take the human choice*/
+
+function getHumanChoice() {
+    return prompt("Rock, Paper or Scissor: ").toLowerCase();
+}
+console.log(getHumanChoice());
